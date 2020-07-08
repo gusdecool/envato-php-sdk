ff05b124fcbd2732e4f5ae45e225ce896779e9fe
Budi Arsana <gusdecool@gmail.com>
2020-07-08 23:01:38 +0800

    Add unit test

--- src/Result/SearchItemsResult.php
+++ src/Result/SearchItemsResult.php
@@ -1,21 +1,27 @@
 <?php
 
+/** @noinspection PhpUnused */
+
 namespace Gusdecool\EnvatoSDK\Result;
 
 use Gusdecool\EnvatoSDK\Result\Links\ResultLinks;
 use Gusdecool\EnvatoSDK\Result\Sub\Item;
 use Gusdecool\EnvatoSDK\Result\Sub\SearchAggregations;
+use JMS\Serializer\Annotation as Serializer;
 
 /**
  * Search items result
  *
  * @see https://build.envato.com/api#search_GET_search_item_json docs
+ * @Serializer\ExclusionPolicy("all")
  */
 class SearchItemsResult
 {
 
     /**
      * Time taken to execute query.
+     * @Serializer\Expose()
+     * @Serializer\Type("int")
      */
     public int $took;
 
@@ -23,6 +29,8 @@ class SearchItemsResult
      * List of found items.
      *
      * @var Item[]
+     * @Serializer\Expose()
+     * @Serializer\Type("array<Gusdecool\EnvatoSDK\Result\Sub\Item>")
      */
     public array $matches = [];
 
--- src/Result/Sub/Item.php
+++ src/Result/Sub/Item.php
@@ -5,96 +5,136 @@
 namespace Gusdecool\EnvatoSDK\Result\Sub;
 
 use Gusdecool\EnvatoSDK\Result\Previews\AllPreviews;
+use JMS\Serializer\Annotation as Serializer;
 
+/**
+ * @Serializer\ExclusionPolicy("all")
+ */
 class Item
 {
     /**
      * item id
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $id;
 
     /**
      * item name
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $name;
 
     /**
      * item description
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $description;
 
     /**
      * full html description
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $descriptionHtml;
 
     /**
      * site item is sold under
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $site;
 
     /**
      * item category
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $classification;
 
     /**
      * link to category page
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $classificationUrl;
 
     /**
      * price of item in cents (if applicable)
+     * @Serializer\Expose()
+     * @Serializer\Type("int")
      */
-    public ?int $priceCents;
+    public ?int $priceCents = null;
 
     /**
      * number of sales made
+     * @Serializer\Expose()
+     * @Serializer\Type("int")
      */
     public int $numberOfSales;
 
     /**
      * username of author
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $authorUsername;
 
     /**
      * link to author profile page
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $authorUrl;
 
     /**
      * link to author image
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $authorImage;
 
     /**
      * url of item
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $url;
 
     /**
      * summary text of item
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $summary;
 
     /**
      * item rating
+     * @Serializer\Expose()
+     * @Serializer\Type("float")
      */
     public float $rating;
 
     /**
      * date item was last updated
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $updatedAt;
 
     /**
      * date item was published
+     * @Serializer\Expose()
+     * @Serializer\Type("string")
      */
     public string $publishedAt;
 
     /**
      * is the item currently trending?
+     * @Serializer\Expose()
+     * @Serializer\Type("bool")
      */
     public bool $trending;
 
@@ -102,6 +142,7 @@ class Item
      * Preview image types
      *
      * @var AllPreviews[]
+     * TODO to expose
      */
     public array $previews = [];
 
@@ -109,6 +150,7 @@ class Item
      * item attribute details
      *
      * @var Attribute[]
+     * TODO to expose
      */
     public array $attributes;
 
@@ -116,6 +158,7 @@ class Item
      * photo attribute details
      *
      * @var Attribute[]
+     * TODO to expose
      */
     public array $photoAttributes = [];
 
@@ -123,6 +166,7 @@ class Item
      * item key features
      *
      * @var string[]
+     * TODO to expose
      */
     public array $keyFeatures = [];
 
@@ -130,6 +174,7 @@ class Item
      * item image url
      *
      * @var Image[]
+     * TODO to expose
      */
     public array $imageUrls = [];
 
@@ -137,6 +182,8 @@ class Item
      * tags for the item
      *
      * @var string[]
+     * @Serializer\Expose()
+     * @Serializer\Type("array<string>")
      */
     public array $tags = [];
 }
